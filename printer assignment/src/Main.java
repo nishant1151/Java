cecf0670ef96d6e56424c9a1e486fe4e895e601f
Nishant <nishantm.1151@gmail.com>
2024-03-12 11:46:26 +0530

    added printer assignment

---
+++ printer assignment/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+    Printer p1=new Printer(50,0,true);
+        System.out.println(p1.addTonner(45));
+
+        System.out.println(p1.printPages(80));
+
+    }
+    }
+
---
+++ printer assignment/src/Printer.java
@@ -0,0 +1,42 @@
+public class Printer {
+   private int tonnerLevel;
+   private int pagesPrinted;
+
+   private boolean duplex;
+
+   public Printer(int tonnerLevel,int pagesPrinted,boolean duplex){
+       this.tonnerLevel=tonnerLevel;
+       this.pagesPrinted=pagesPrinted;
+       this.duplex=duplex;
+
+
+   }
+   public int addTonner(int ink){
+
+       if(tonnerLevel>0 && tonnerLevel<=100 && ink>0 && ink<100) {
+           System.out.println(tonnerLevel);
+
+           tonnerLevel = tonnerLevel + ink;
+       }
+       return tonnerLevel;
+   }
+   public int printPages(int pages){
+       if(tonnerLevel>0 && tonnerLevel<pages){
+       if(duplex ){
+           pagesPrinted=pages/2;
+           if(pages%2!=0){
+           ++pagesPrinted;
+           }
+       }
+       else{
+           pagesPrinted=pages;
+       }
+       tonnerLevel--;
+       return pagesPrinted;
+   }
+       else {
+           System.out.println("tonner level low");
+           return 0;
+       }
+   }
+}
